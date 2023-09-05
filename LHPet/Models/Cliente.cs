376d30b7c3479557b8d0f3b817f343117dba3f38
Kennedy Klaydm <klaydm@gmail.com>
2023-09-05 20:30:38 -0300

    Projeto concluido, finalizados o View e Controller

--- LHPet/Models/Cliente.cs
+++ LHPet/Models/Cliente.cs
@@ -2,7 +2,7 @@ namespace LHPet.Models;
 
 public class Cliente
 {
-    public class Id { get; set; }
+    public int Id { get; set; }
     public string Nome { get; set; }
     public string Cpf { get; set; }
     public string Email { get; set; }
